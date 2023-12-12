548217fdddf6e3b32711b64b02f592c8799e2ace
pikachusyellow <jjmaxduke@gmail.com>
2023-12-12 17:58:35 -0500

    adding in information on data clean up of measurments.csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,22 @@ def run():
             ## Cleaning up person.csv
 
             ## Cleaning up measurements.csv
+            
+            Due to the immense size of the csv, and RAM limitations of the app + resources, we took only a subselection of the measurements by a patient to display. Here we took only 1 million measurements from the original csv, and then began data cleaning on this subselection. To host and visualize the data in the timeline format required, conversion of the data needed to be performed, subsetting each by the patient and converting into a [timeline json format](https://timeline.knightlab.com/docs/json-format.html). 
 
+            In terms of additional cleaning done by the 1 Million measurments that were subselected, many of these actually were missing dates and had to be dropped. There were 4 different categories of measurments that were taken:
+            
+            | Concept ID | Counts | Counts with dates |
+            |-----|---|---|
+            |Clinical Observation|203852|203852|
+            |Lab Test|600051|502838|
+            |Observable Entity|195875|195875|
+            |Procedure|222|181|
+            |**Total**|**1000000**|**902756**|
+
+            Each category was used as the title for each "event" in the timeline with the observable entry acting as the textual data. Once completed, data was pulled from the cleaned person.csv and general patient information was displayed on the summary page of the timeline as well as adding the death tag and death date if the patient had past. Data cleaning was performed in R and a small subselection of patients were converted into timeline json and displayed onto the timeline tab. R Scripting of the data clean up is available in the repo under [measurements_cleanup.R](https://github.com/MaxLChao/streamlit-webspace/blob/main/measurenments_cleanup.R)
+
+            
             ## Cleaning up condition_occurence.csv
 
 
